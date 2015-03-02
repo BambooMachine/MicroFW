ba72d543313cb184048d2560eeb9e06c4f7a654d
Lukas Pohlreich <pohlreichlukas@gmail.com>
2015-03-02 16:45:24 +0100

    Added Application class

---
+++ MicroFW/Application.php
@@ -0,0 +1,17 @@
+<?php
+namespace MicroFW;
+
+use MicroFW\Http\Request;
+
+class Application
+{
+    public static function setup()
+    {
+        $host = $_SERVER['HTTP_HOST'];
+        $port = $_SERVER['SERVER_PORT'];
+        $headers = getallheaders();
+        $method = $_SERVER['REQUEST_METHOD'];
+        $path = $_SERVER['PATH_INFO'];
+        $request = new Request($host, $port, $headers, $path, $method);
+    }
+}
