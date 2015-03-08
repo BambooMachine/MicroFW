abee31e782760ca4a3eae521b57d13849fac29f6
Lukas Pohlreich <pohlreichlukas@gmail.com>
2015-03-08 14:07:08 +0100

    TemplateController for simple template response

---
+++ MicroFW/Controllers/TemplateController.php
@@ -0,0 +1,25 @@
+<?php
+namespace MicroFW\Controllers;
+
+use MicroFW\Controllers\Controller;
+use MicroFW\Templating\Template;
+use MicroFW\Http\Response;
+
+class TemplateController extends Controller
+{
+    protected $templateName;
+
+    public function GET()
+    {
+        $context = $this->getContext();
+
+        return $this->renderAndResponse($context);
+    }
+
+    private function renderAndResponse($context)
+    {
+        $template = new Template($this->templateName);
+
+        return new Response($template->render($context));
+    }
+}
